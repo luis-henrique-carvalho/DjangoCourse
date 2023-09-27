a5de2177afa8f629d78c4c172f568cc9cbe60384
luis-henrique-carvalho <luisdev9225@gmail.com>
2023-09-27 16:18:16 -0300

    :sparkles: feat: template class

--- blog/website/views.py
+++ blog/website/views.py
@@ -3,4 +3,9 @@ from django.http import HttpResponse
 
 # Create your views here.
 def hello_blog(request):
-    return render(request, 'index.html')
+    lista = ["Django", "python", "Git", "Html",
+             "Bando de daods",
+             "linux", "Nginx"]
+    
+    data = {"name": "Curso de Django 3", "lista": lista}
+    return render(request, 'index.html', data)
